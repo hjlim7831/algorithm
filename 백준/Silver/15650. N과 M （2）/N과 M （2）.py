e5db5f8eab00cf4ee19f182b7d0ac9c5c070defe
hjlim7831 <103012931+hjlim7831@users.noreply.github.com>
2023-02-27 20:35:47 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,18 +1,8 @@
 import sys
+from itertools import combinations
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
-def comb(sidx:int, idx:int, res:list):
-    global N, M
-    if sidx == M:
-        print(*res)
-        return
-    if idx == N:
-        return
-    res.append(idx+1)
-    comb(sidx+1, idx+1, res)
-    res.remove(res[-1])
-    comb(sidx, idx+1, res)
-
-comb(0, 0, [])
+for res in combinations([i for i in range(1, N+1)], M):
+    print(*res)
