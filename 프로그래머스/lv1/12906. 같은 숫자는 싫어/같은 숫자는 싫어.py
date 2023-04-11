6aaece9df61b5317daff87778286b6a1150b8f84
hjlim7831 <103012931+hjlim7831@users.noreply.github.com>
2023-04-11 09:20:18 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 67.35 ms, Memory: 27.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    answer = []
+    for num in arr:
+        if answer:
+            if answer[-1] != num:
+                answer.append(num)
+        else:
+            answer.append(num)
+    
+    
+    return answer
