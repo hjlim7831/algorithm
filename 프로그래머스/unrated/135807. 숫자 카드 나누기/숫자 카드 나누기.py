0e72dac8cde3478a5220632c9895ea8a1a8d6cef
hjlim7831 <103012931+hjlim7831@users.noreply.github.com>
2023-07-04 13:38:35 +0900

    [unrated] Title: 숫자 카드 나누기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.py
+++ 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -6,46 +6,24 @@ def solution(arrayA, arrayB):
     
     for i in range(1, len(arrayB)):
         gcd_b = euclidean(gcd_b, arrayB[i])
-    
-    a_nums, b_nums = set(), set()
-    for i in range(1, int(gcd_a**0.5) + 1):
-        if gcd_a % i == 0:
-            a_nums.add(i)
-            a_nums.add(gcd_a // i)
-    
-    for i in range(1, int(gcd_b**0.5 + 1)):
-        if gcd_b % i == 0:
-            b_nums.add(i)
-            b_nums.add(gcd_b // i)
-    a_nums, b_nums = list(a_nums), list(b_nums)
-    a_nums.sort(reverse=True)
-    b_nums.sort(reverse=True)
-    
+        
     answer_a = 0
-    for div in a_nums:
-        if div == 1:
+    answer_flag = True
+    for num in arrayB:
+        if num % gcd_a == 0:
+            answer_flag = False
             break
-        answer_flag = True
-        for num in arrayB:
-            if num % div == 0:
-                answer_flag = False
-                break
-        if answer_flag:
-            answer_a = div
-            break
+    if answer_flag:
+        answer_a = gcd_a
     
     answer_b = 0
-    for div in b_nums:
-        if div == 1:
+    answer_flag = True
+    for num in arrayA:
+        if num % gcd_b == 0:
+            answer_flag = False
             break
-        answer_flag = True
-        for num in arrayA:
-            if num % div == 0:
-                answer_flag = False
-                break
-        if answer_flag:
-            answer_b = div
-            break
+    if answer_flag:
+        answer_b = gcd_b
     return max(answer_a, answer_b)
 
 def euclidean(a, b):
