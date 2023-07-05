ae588f412a48245af2e967818cf4874cca724a30
hjlim7831 <103012931+hjlim7831@users.noreply.github.com>
2023-07-05 14:53:41 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.00 ms, Memory: 9.87 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,15 @@
+def solution(number, k):
+    stack = []
+    for num in number:
+        if not stack:
+            stack.append(num)
+            continue
+        if k > 0:
+            while stack[-1] < num:
+                stack.pop()
+                k -= 1
+                if not stack or k == 0:
+                    break
+        stack.append(num)
+    answer = stack[:-k] if k > 0 else stack
+    return "".join(answer)
