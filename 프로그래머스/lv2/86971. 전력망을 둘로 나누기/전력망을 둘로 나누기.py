7eea492644c0117b4c50f6052269dcd519f9c795
hjlim7831 <103012931+hjlim7831@users.noreply.github.com>
2023-07-12 19:57:37 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 259.29 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,36 +1,9 @@
 def solution(n, wires):
-    adj_list = [[] for _ in range(n+1)]
-    for v1, v2 in wires:
-        adj_list[v1].append(v2)
-        adj_list[v2].append(v1)
-    
-    answer = n
-    for v1, v2 in wires:
-        visited = [False] * (n+1)
-        stack = [v1]
-        vn1 = 0
-        while stack:
-            now = stack.pop()
-            visited[now] = True
-            vn1 += 1
-            for nex in adj_list[now]:
-                if nex == v2:
-                    continue
-                if visited[nex]:
-                    continue
-                stack.append(nex)
-        vn2 = 0
-        stack = [v2]
-        while stack:
-            now = stack.pop()
-            visited[now] = True
-            vn2 += 1
-            for nex in adj_list[now]:
-                if nex == v1:
-                    continue
-                if visited[nex]:
-                    continue
-                stack.append(nex)
-        answer = min(abs(vn1 - vn2), answer)
-            
-    return answer
+    ans = n
+    for sub in (wires[i+1:] + wires[:i] for i in range(len(wires))):
+        s = set(sub[0])
+        print(s)
+        [s.update(v) for _ in sub for v in sub if set(v) & s]
+        print(s)
+        ans = min(ans, abs(2 * len(s) - n))
+    return ans
