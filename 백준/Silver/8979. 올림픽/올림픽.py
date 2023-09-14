8b18148aa2dd7fb98069f6bd8d81ef2d0d6cd6df
hjlim7831 <103012931+hjlim7831@users.noreply.github.com>
2023-09-14 19:59:57 +0900

    [Silver V] Title: 올림픽, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+countries = [tuple(map(int, input().split())) for _ in range(N)]
+
+for country in countries:
+    if country[0] == K:
+        target = country
+
+rank = 1
+for country in countries:
+    if target[1] > country[1]:
+        rank += 1
+    elif target[1] == country[1] and target[2] > country[2]:
+        rank += 1
+    elif target[1] == country[1] and target[2] == country[2] and target[3] > country[3]:
+        rank += 1
+
+print(rank)
