aa40668b7f4c5c8680b16693f09a30a190e11c0b
hjlim7831 <103012931+hjlim7831@users.noreply.github.com>
2023-12-09 16:30:23 +0900

    [Bronze IV] Title: No Brainer, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+for _ in range(N):
+    x, y = map(int, input().split())
+    if x < y:
+        print("NO BRAINS")
+    else:
+        print("MMM BRAINS")
