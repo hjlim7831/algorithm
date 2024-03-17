ce83590dc63bb9ce1ee936d5027e77ec0729d1b6
hjlim7831 <103012931+hjlim7831@users.noreply.github.com>
2024-03-17 09:25:48 +0900

    Time: 133 ms (33.31%), Space: 65.8 MB (58.42%) - LeetHub

---
+++ LeetCode/2724-sort-by/2724-sort-by.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+    
+    arr.sort((a, b) => fn(a) - fn(b))
+    
+    return arr;
+    
+};
