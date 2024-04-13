3f221735ab46131f2297745cf2007f08b1df05ac
hjlim7831 <103012931+hjlim7831@users.noreply.github.com>
2024-04-13 09:54:40 +0900

    Time: 1318 ms (35.68%), Space: 0B (100%) - LeetHub

--- LeetCode/1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ LeetCode/1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -3,12 +3,23 @@
 --     SELECT *, ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date DESC) AS rn
 --     FROM Delivery d
 -- )
+-- WITH DeliveryFirstOrders AS (
+--     SELECT customer_id, min(order_date) AS first_order_date
+--     FROM Delivery d
+--     GROUP BY customer_id
+-- )
+-- SELECT ROUND(AVG(IF(dfo.first_order_date = d.customer_pref_delivery_date, 100, 0)), 2) AS immediate_percentage
+-- FROM DeliveryFirstOrders dfo INNER JOIN Delivery d
+-- WHERE d.customer_id = dfo.customer_id AND d.order_date = dfo.first_order_date
+-- ;
+-- SELECT d.customer_id, first_order_date, customer_pref_delivery_date FROM DeliveryFirstOrders dfo INNER JOIN Delivery d
+-- WHERE d.customer_id = dfo.customer_id
+-- AND d.order_date = dfo.first_order_date
+
 WITH DeliveryFirstOrders AS (
-    SELECT customer_id, min(order_date) AS first_order_date
+    SELECT *, ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date ASC) AS rn
     FROM Delivery d
-    GROUP BY customer_id
 )
-SELECT ROUND(AVG(IF(dfo.first_order_date = d.customer_pref_delivery_date, 100, 0)), 2) AS immediate_percentage
-FROM DeliveryFirstOrders dfo INNER JOIN Delivery d
-WHERE d.customer_id = dfo.customer_id AND d.order_date = dfo.first_order_date
-;
+SELECT ROUND(AVG(IF(dfo.order_date = dfo.customer_pref_delivery_date, 100, 0)), 2) AS immediate_percentage
+FROM DeliveryFirstOrders dfo
+WHERE rn = 1;
